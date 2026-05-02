063080393995c6392547314028e23e6d93c942db
PS_06 <ps01091977@gmail.com>
2026-05-02 23:51:55 +0530

    solve 2

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: 'https://notevaultpro3-o-backend.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
-  // We'll get token from Zustand store instead of localStorage directly
   const state = JSON.parse(localStorage.getItem('auth-storage'));
   const token = state?.state?.user?.token;
   
